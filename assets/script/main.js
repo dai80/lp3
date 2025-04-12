6eb4b2c45f310bcbda80b078cd31b4a68999e76a
dai80 <bwtrb80@yahoo.co.jp>
2025-04-12 20:59:24 +0900

    修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,25 +1,18 @@
 // swiper
 document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".voice__carousel", {
-      loop: false,
-      spaceBetween: 15,
-      // navigation: {
-      //     nextEl: ".swiper-button-next",
-      //     prevEl: ".swiper-button-prev"
-      // },
-      pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-      },
-      breakpoints: {
-          768: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-          },
-          769: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-          }
+    loop: false,
+    spaceBetween: 15,
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
       }
+    }
   });
 });
