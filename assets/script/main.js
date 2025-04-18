56d846e7556e800b8c058dd0945910b8f1c7f5e2
dai80 <bwtrb80@yahoo.co.jp>
2025-04-18 22:34:40 +0900

    再修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,15 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: false,
     spaceBetween: 15,
     slidesPerView: 1.2,
+    slidesPerGroup: 1,
     centeredSlides: true,
+    watchOverflow: false,
+
     pagination: {
       el: ".swiper-pagination",
-      clickable: true
+      clickable: true,
+      type: "bullets",
     },
+
     breakpoints: {
       768: {
         slidesPerView: 3,
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        loop: true,
       }
     }
   });
 });
+
+
